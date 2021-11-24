8761411b838a32208b5897b71e1f30a4ec8197bf
Sachin Naranji <snaranji01@gmail.com>
2021-11-24 20:58:53 +0000

    Expands filter by category functionality by adding ability to specify a reviewcard property to sort by, and choose a sort order. Also updates written format of categories, sortby options and order options from their backend names.

--- src/components/ListReviews/ListReviews.tsx
+++ src/components/ListReviews/ListReviews.tsx
@@ -3,7 +3,7 @@ import { IFilterParams, singleReviewObj } from "../../App";
 import ReviewListingCard from "./ReviewListingCard";
 import "./ListReviews.css";
 import { getReviews } from "../../api";
-import { useLocation } from "react-router-dom";
+import { StringLiteral } from "@babel/types";
 
 interface IProps {
     allCategories: string[];
@@ -25,38 +25,58 @@ const ListReviews: React.FC<IProps> = ({ allCategories, filterParams, setFilterP
 
     // sort-filter-panel
     // 1) category
+    interface FormatCategoriesRefObj {
+        [backendName: string]: string
+    }
+
+    const formatCategoriesRefObj: FormatCategoriesRefObj =  {
+        "strategy": "Strategy",
+        "hidden-roles": "Hidden Roles",
+        "dexterity": "Dexterity",
+        "push-your-luck": "Push Your Luck",
+        "roll-and-write": "Roll And Write",
+        "deck-building": "Deck Building",
+        "engine-building": "Engine Building"
+    };
+
     const setCategoryParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
         const chosenCategory = (e.target as HTMLButtonElement).value
         setFilterParams(filterParams => ({ ...filterParams, category: chosenCategory }))
     }
     // 2) sort-by
-    const allSortBy = ["title", "designer", "owner", "category", "created_at", "review_votes"];
+    const allSortBy = [
+        { backendName: "title", frontendName: "Title" },
+        { backendName: "owner", frontendName: "Author" },
+        { backendName: "category", frontendName: "Category" },
+        { backendName: "created_at", frontendName: "Created at" },
+        { backendName: "review_votes", frontendName: "Upvotes" },
+    ]
+
     const setSortByParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
         const chosenSortBy = (e.target as HTMLButtonElement).value
         setFilterParams(filterParams => ({ ...filterParams, sortBy: chosenSortBy }))
     }
     // 3) order
-    const allOrder = ["asc", "desc"];
+    const allOrder = [
+        { backendName: "asc", frontendName: "Ascending" },
+        { backendName: "desc", frontendName: "Descending" }
+    ];
     const setOrderParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
         const chosenOrder = (e.target as HTMLButtonElement).value
         setFilterParams(filterParams => ({ ...filterParams, order: chosenOrder }))
     }
 
     return (
-        <div className="reviews-page-container">
-            <div className="page-title">
-                <h1 id="reviews-page-title">Reviews</h1>
-            </div>
-
+        <div className="list-reviews-page-container">
             <div className="sort-filter-panel">
                 <form>
-                    <legend>Choose a Category to filter by:</legend>
+                    <legend>Filter by category:</legend>
                     <fieldset>
                         {
                             allCategories.map(category => {
                                 return (
                                     <div key={`radio-${category}-option`} className="sorting-panel-category">
-                                        <label htmlFor="category-choose-radio">{category}</label>
+                                        <label htmlFor="category-choose-radio">{formatCategoriesRefObj[category]}</label>
                                         <input
                                             type="radio"
                                             id={`radio-${category}-option`} value={category}
@@ -68,16 +88,16 @@ const ListReviews: React.FC<IProps> = ({ allCategories, filterParams, setFilterP
                             })
                         }
                     </fieldset>
-                    <legend>Choose a column to sort by:</legend>
+                    <legend>Sort by:</legend>
                     <fieldset>
                         {
                             allSortBy.map(sortby => {
                                 return (
-                                    <div key={`radio-${sortby}-option`} className="sorting-panel-sortby">
-                                        <label htmlFor="sortby-choose-radio">{sortby}</label>
+                                    <div key={`radio-${sortby.backendName}-option`} className="sorting-panel-sortby">
+                                        <label htmlFor="sortby-choose-radio">{sortby.frontendName}</label>
                                         <input
                                             type="radio"
-                                            id={`radio-${sortby}-option`} value={sortby}
+                                            id={`radio-${sortby.backendName}-option`} value={sortby.backendName}
                                             name="choose-sortby-option"
                                             onClick={setSortByParamHandler}
                                         />
@@ -91,11 +111,11 @@ const ListReviews: React.FC<IProps> = ({ allCategories, filterParams, setFilterP
                         {
                             allOrder.map(order => {
                                 return (
-                                    <div key={`radio-${order}-option`} className="sorting-panel-order">
-                                        <label htmlFor="order-choose-radio">{order}</label>
+                                    <div key={`radio-${order.backendName}-option`} className="sorting-panel-order">
+                                        <label htmlFor="order-choose-radio">{order.frontendName}</label>
                                         <input
                                             type="radio"
-                                            id={`radio-${order}-option`} value={order}
+                                            id={`radio-${order}-option`} value={order.backendName}
                                             name="choose-order-option"
                                             onClick={setOrderParamHandler}
                                         />
@@ -106,14 +126,17 @@ const ListReviews: React.FC<IProps> = ({ allCategories, filterParams, setFilterP
                     </fieldset>
                 </form>
             </div>
-
             <div id="list-reviews-container">
-                {
-                    reviewListData.map(reviewData => {
-                        return <ReviewListingCard key={reviewData.review_id} reviewData={reviewData} />
-                    })
-                }
+                <h1 id="reviews-page-title">Reviews</h1>
+                <div id="list-review-cards-container">
+                    {
+                        reviewListData.map(reviewData => {
+                            return <ReviewListingCard key={reviewData.review_id} reviewData={reviewData} />
+                        })
+                    }
+                </div>
             </div>
+
         </div>
     )
 }
