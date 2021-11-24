10d626b8d81a9d6ff50873f860dc658d4261beae
Sachin Naranji <snaranji01@gmail.com>
2021-11-24 23:53:33 +0000

    Data fetched via getReviewById api function. Now renders review information.

--- src/components/SingleReview.tsx
+++ src/components/SingleReview.tsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getReviewById } from "../api";
+import { singleReviewObj } from "../App";
+
 const SingleReview = () => {
-    return(
-        <h1>Northcoders Games</h1>
+    const { review_id } = useParams();
+
+    const [singleReviewData, setSingleReviewData] = useState<singleReviewObj>({
+        review_id: 0,
+        title: "",
+        owner: "",
+        category: "",
+        designer: "",
+        review_img_url: "",
+        review_body: "",
+        created_at: "",
+        review_votes: 0,
+        comment_count: 0,
+    });
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+
+    useEffect(() => {
+        getReviewById(review_id)
+            .then(response => {
+                setSingleReviewData(response)
+                setIsLoading(false);
+                console.log('here')
+            })
+    }, [])
+
+    return (
+        !isLoading ? (
+            <div className="singleReviewPageContainer">
+                <div className="article-content">
+                    <h2>{singleReviewData.title}</h2>
+                    <p>Author: {singleReviewData.owner}</p>
+                    <p>Categories: {singleReviewData.category}</p>
+                    <p>Designer: {singleReviewData.designer}</p>
+                    <p>Review Body: {singleReviewData.review_body}</p>
+                    <p>Created at: {singleReviewData.created_at}</p>
+                    <p>Review votes: {singleReviewData.review_votes}</p>
+                    <p>Comment count: {singleReviewData.comment_count}</p>
+                    <img src={singleReviewData.review_img_url} alt="The game being reviewed" />
+                </div>
+            </div>
+        ) : <p>Loading...</p>
+
+
+
     )
 }
 
