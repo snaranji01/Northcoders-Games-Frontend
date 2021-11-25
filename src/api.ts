27f569b7c0570513ce617c1b9fe0ba7931005456
Sachin Naranji <snaranji01@gmail.com>
2021-11-25 12:40:22 +0000

    Removed unused interface definition

--- src/api.ts
+++ src/api.ts
@@ -5,12 +5,6 @@ const ncgamesAPI: AxiosInstance = axios.create({
     baseURL: "https://nc-games-backend-snaranji01.herokuapp.com/api"
 });
 
-interface IParams {
-    sort_by: string;
-    order: string;
-    category: string;
-}
-
 export const getReviews = (filterParams: IFilterParams) => {
 
     const params = {
@@ -43,5 +37,13 @@ export const getCategories = () => {
         .get('/categories')
         .then(response => {
             return response.data.categories;
+        })
+}
+
+export const getUsers = () => {
+    return ncgamesAPI
+        .get('/users')
+        .then(response => {
+            return response.data.users;
         })
 }
