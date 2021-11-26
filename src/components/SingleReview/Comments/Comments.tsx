4492781cbc141718f862b4cccd2bfd7f93322cf4
Sachin Naranji <snaranji01@gmail.com>
2021-11-26 15:29:58 +0000

    extracts Comments component to it's own folder. adds upvote comment icon

--- src/components/SingleReview/Comments/Comments.tsx
+++ src/components/SingleReview/Comments/Comments.tsx
@@ -1,7 +1,11 @@
 import { useContext, useEffect, useState } from "react"
-import { getReviewCommentsById, postReviewComment } from "../../api"
-import { singleReviewObj } from "../../App";
-import { UserContext } from "../../contexts/User";
+import { GoArrowUp } from 'react-icons/go';
+
+import { getReviewCommentsById, postReviewComment } from "../../../api"
+import { singleReviewObj } from "../../../App";
+import { UserContext } from "../../../contexts/User";
+
+import './Comments.css';
 
 interface IProps {
     review_id: string | undefined;
@@ -21,7 +25,7 @@ const Comments: React.FC<IProps> = ({ review_id }) => {
     const { currentUser } = useContext(UserContext);
 
     //util
-    const formatCreatedAt = (serverResponseString: string): string => {
+    const formatCreatedAtComment = (serverResponseString: string): string => {
         const dateObj = new Date(Date.parse(serverResponseString));
         const timeDateStr = {
             year: dateObj.getFullYear(),
@@ -66,13 +70,25 @@ const Comments: React.FC<IProps> = ({ review_id }) => {
             })
     }
 
-
+    const sortCommentOptions = [
+        { keyName: "author", displayName: "Author" },
+        { keyName: "comment_votes", displayName: "Upvotes" },
+        { keyName: "created_at", displayName: "Created at" }
+    ];
+    /* const sortCommentsHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+        const sortBy: string = (e.target as HTMLButtonElement).value;
+        setReviewComments(reviewComments => {
+            const reviewCommentsCopy = [...reviewComments];
+            const sortedComments = reviewCommentsCopy.sort((a,b) => a[sortBy]-b[sortBy]);
+            return sortedComments;
+        }
+    } */
     return (
         <>
             <h2>Comments</h2>
             <div className="add-comment">
                 <form onSubmit={postCommentHandler}>
-                    <label htmlFor="add-comment-input">Add a comment: </label>
+                    <label htmlFor="add-comment-input">Post a comment as <span style={{ fontWeight: "bold" }}>{currentUser.username}</span>: </label>
                     <input
                         type="text"
                         name="add-comment-input"
@@ -87,14 +103,41 @@ const Comments: React.FC<IProps> = ({ review_id }) => {
                 reviewComments.length !== 0 ? (
                     !isLoadingComments ? (
                         <div className="comments-list">
+                            <div className="sort-comments">
+                                <form>
+                                    {
+                                        sortCommentOptions.map(sortCommentOption => {
+                                            <div className="comment-sort-option">
+                                                <label htmlFor={`sort-by-${sortCommentOption.keyName}`}>{sortCommentOption.displayName}</label>
+                                                <input
+                                                    type="radio"
+                                                    name="sort-comments-option"
+                                                    id={`sort-by-${sortCommentOption.keyName}`}
+                                                    value={sortCommentOption.keyName}
+                                                    // onClick={sortCommentsHandler}
+                                                />
+                                            </div>
+                                        })
+                                    }
+
+
+
+                                </form>
+
+
+                            </div>
                             {
                                 reviewComments.map(reviewComment => {
                                     return (
                                         <div className="review-comment">
-                                            <p>{reviewComment.author}</p>
-                                            <p>{formatCreatedAt(reviewComment.created_at)}</p>
+                                            <h3>{reviewComment.author}</h3>
+                                            <p>Date Posted: {formatCreatedAtComment(reviewComment.created_at)}</p>
                                             <p>{reviewComment.body}</p>
-                                            <p>{reviewComment.comment_votes}</p>
+                                            <div className="comment-upvotes-container">
+                                                <p className="comment-upvotes">{reviewComment.comment_votes}</p>
+                                                <p className="comment-upvotes"><GoArrowUp /></p>
+                                            </div>
+
                                         </div>
                                     )
                                 })
@@ -102,7 +145,6 @@ const Comments: React.FC<IProps> = ({ review_id }) => {
                         </div>
                     ) : <p>Loading...</p>
                 ) : <p>No existing comments</p>
-
             }
         </>
     )
