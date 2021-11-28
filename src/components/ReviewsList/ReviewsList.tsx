7691095f5b535ea98a0ae363a88fb41cede79bf8
snaranji01 <snaranji01@gmail.com>
2021-11-28 15:30:42 +0000

    extracts out SortFilterPanel component and it's styling to a separate component

--- src/components/ReviewsList/ReviewsList.tsx
+++ src/components/ReviewsList/ReviewsList.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState, Dispatch, SetStateAction } from "react";
 
 import "./ReviewsList.css";
 
-import ReviewsListCard from "./ReviewsListCard/ReviewsListCard";
-
 import { getReviews } from "../../api";
 
 import { IFilterParams, SingleReviewObj } from "../../types/types";
-import { formatCategoryNames } from "../../utils/utils";
+
+import ReviewsListCard from "./ReviewsListCard/ReviewsListCard";
+import SortFilterPanel from "./SortFilterPanel/SortFilterPanel";
 
 interface IProps {
     allCategories: string[];
@@ -28,116 +28,9 @@ const ReviewsList: React.FC<IProps> = ({ allCategories, filterParams, setFilterP
             })
             .catch(console.log)
     }, [filterParams])
-
-
-// sort-filter-panel
-    // 1) category
-    const setCategoryParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-        const chosenCategory = (e.target as HTMLButtonElement).value
-        setFilterParams(filterParams => ({ ...filterParams, category: chosenCategory }))
-    }
-    // 2) sort-by
-    const allSortBy = [
-        { backendName: "title", frontendName: "Title" },
-        { backendName: "owner", frontendName: "Author" },
-        { backendName: "category", frontendName: "Category" },
-        { backendName: "created_at", frontendName: "Created at" },
-        { backendName: "review_votes", frontendName: "Upvotes" },
-    ]
-
-    const setSortByParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-        const chosenSortBy = (e.target as HTMLButtonElement).value
-        setFilterParams(filterParams => ({ ...filterParams, sortBy: chosenSortBy }))
-    }
-    // 3) order
-    const allOrder = [
-        { backendName: "asc", frontendName: "Ascending" },
-        { backendName: "desc", frontendName: "Descending" }
-    ];
-    const setOrderParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-        const chosenOrder = (e.target as HTMLButtonElement).value
-        setFilterParams(filterParams => ({ ...filterParams, order: chosenOrder }))
-    }
-
-    // toggle sort-filter-panel
-    const [toggleSortFilterPanel, setToggleSortFilterPanel] = useState<boolean>(false);
-    const ToggleSortFilterPanelHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        setToggleSortFilterPanel(toggleSortFilterPanel => !toggleSortFilterPanel);
-        const currentText = (e.target as HTMLButtonElement).textContent;
-        if (currentText === "Click to sort and filter") {
-            (e.target as HTMLButtonElement).textContent = "Click to minimise"
-        } else {
-            (e.target as HTMLButtonElement).textContent = "Click to sort and filter"
-        }
-
-    }
     return (
         <div className="list-reviews-page-container">
-            <div className="sort-filter-panel">
-                <button onClick={ToggleSortFilterPanelHandler} className="toggleSortFilterPanel">
-                    {'Click to sort and filter'}
-                </button>
-                {
-                    toggleSortFilterPanel ? (
-                        <form id="sort-filter-form">
-                            <legend>Filter by category:</legend>
-                            <fieldset>
-                                {
-                                    allCategories.map(category => {
-                                        return (
-                                            <div key={`radio-${category}-option`} className="sorting-panel-category">
-                                                <label htmlFor="category-choose-radio">{formatCategoryNames(category)}</label>
-                                                <input
-                                                    type="radio"
-                                                    id={`radio-${category}-option`} value={category}
-                                                    name="choose-category-option"
-                                                    onClick={setCategoryParamHandler}
-                                                />
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </fieldset>
-                            <legend>Sort by:</legend>
-                            <fieldset>
-                                {
-                                    allSortBy.map(sortby => {
-                                        return (
-                                            <div key={`radio-${sortby.backendName}-option`} className="sorting-panel-sortby">
-                                                <label htmlFor="sortby-choose-radio">{sortby.frontendName}</label>
-                                                <input
-                                                    type="radio"
-                                                    id={`radio-${sortby.backendName}-option`} value={sortby.backendName}
-                                                    name="choose-sortby-option"
-                                                    onClick={setSortByParamHandler}
-                                                />
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </fieldset>
-                            <legend>Order:</legend>
-                            <fieldset>
-                                {
-                                    allOrder.map(order => {
-                                        return (
-                                            <div key={`radio-${order.backendName}-option`} className="sorting-panel-order">
-                                                <label htmlFor="order-choose-radio">{order.frontendName}</label>
-                                                <input
-                                                    type="radio"
-                                                    id={`radio-${order}-option`} value={order.backendName}
-                                                    name="choose-order-option"
-                                                    onClick={setOrderParamHandler}
-                                                />
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </fieldset>
-                        </form>
-                    ) : null
-                }
-            </div>
+            <SortFilterPanel filterParams={filterParams} setFilterParams={setFilterParams} allCategories={allCategories} />
             <div className="header-and-reviews">
                 <h1 id="header">Northcoders Games</h1>
                 <div id="list-reviews-container">
---
+++ src/components/ReviewsList/SortFilterPanel/SortFilterPanel.tsx
@@ -0,0 +1,123 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import { IFilterParams } from "../../../types/types";
+import { formatCategoryNames } from "../../../utils/utils";
+import './SortFilterPanel.css';
+
+interface IProps {
+    filterParams: IFilterParams;
+    setFilterParams: Dispatch<SetStateAction<IFilterParams>>;
+    allCategories: string[];
+}
+
+const SortFilterPanel: React.FC<IProps> = ({ filterParams, setFilterParams, allCategories }) => {
+    // sort-filter-panel
+    // 1) category
+    const setCategoryParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+        const chosenCategory = (e.target as HTMLButtonElement).value
+        setFilterParams(filterParams => ({ ...filterParams, category: chosenCategory }))
+    }
+    // 2) sort-by
+    const allSortBy = [
+        { backendName: "title", frontendName: "Title" },
+        { backendName: "owner", frontendName: "Author" },
+        { backendName: "category", frontendName: "Category" },
+        { backendName: "created_at", frontendName: "Created at" },
+        { backendName: "review_votes", frontendName: "Upvotes" },
+    ]
+
+    const setSortByParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+        const chosenSortBy = (e.target as HTMLButtonElement).value
+        setFilterParams(filterParams => ({ ...filterParams, sortBy: chosenSortBy }))
+    }
+    // 3) order
+    const allOrder = [
+        { backendName: "asc", frontendName: "Ascending" },
+        { backendName: "desc", frontendName: "Descending" }
+    ];
+    const setOrderParamHandler = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+        const chosenOrder = (e.target as HTMLButtonElement).value
+        setFilterParams(filterParams => ({ ...filterParams, order: chosenOrder }))
+    }
+
+    // toggle sort-filter-panel
+    const [toggleSortFilterPanel, setToggleSortFilterPanel] = useState<boolean>(false);
+    const ToggleSortFilterPanelHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        setToggleSortFilterPanel(toggleSortFilterPanel => !toggleSortFilterPanel);
+        const currentText = (e.target as HTMLButtonElement).textContent;
+        if (currentText === "Click to sort and filter") {
+            (e.target as HTMLButtonElement).textContent = "Click to minimise"
+        } else {
+            (e.target as HTMLButtonElement).textContent = "Click to sort and filter"
+        }
+    }
+
+    return (
+        < div className="sort-filter-panel" >
+            <button onClick={ToggleSortFilterPanelHandler} className="toggleSortFilterPanel">
+                {'Click to sort and filter'}
+            </button>
+            {
+                toggleSortFilterPanel ? (
+                    <form id="sort-filter-form">
+                        <legend>Filter by category:</legend>
+                        <fieldset>
+                            {
+                                allCategories.map(category => {
+                                    return (
+                                        <div key={`radio-${category}-option`} className="sorting-panel-category">
+                                            <label htmlFor="category-choose-radio">{formatCategoryNames(category)}</label>
+                                            <input
+                                                type="radio"
+                                                id={`radio-${category}-option`} value={category}
+                                                name="choose-category-option"
+                                                onClick={setCategoryParamHandler}
+                                            />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </fieldset>
+                        <legend>Sort by:</legend>
+                        <fieldset>
+                            {
+                                allSortBy.map(sortby => {
+                                    return (
+                                        <div key={`radio-${sortby.backendName}-option`} className="sorting-panel-sortby">
+                                            <label htmlFor="sortby-choose-radio">{sortby.frontendName}</label>
+                                            <input
+                                                type="radio"
+                                                id={`radio-${sortby.backendName}-option`} value={sortby.backendName}
+                                                name="choose-sortby-option"
+                                                onClick={setSortByParamHandler}
+                                            />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </fieldset>
+                        <legend>Order:</legend>
+                        <fieldset>
+                            {
+                                allOrder.map(order => {
+                                    return (
+                                        <div key={`radio-${order.backendName}-option`} className="sorting-panel-order">
+                                            <label htmlFor="order-choose-radio">{order.frontendName}</label>
+                                            <input
+                                                type="radio"
+                                                id={`radio-${order}-option`} value={order.backendName}
+                                                name="choose-order-option"
+                                                onClick={setOrderParamHandler}
+                                            />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </fieldset>
+                    </form>
+                ) : null
+            }
+        </div >
+    )
+}
+
+export default SortFilterPanel
