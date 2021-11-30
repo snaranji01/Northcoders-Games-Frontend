bbf49607a0e0b3cc1ec94c35fe8c75793da96139
Sachin Naranji <snaranji01@gmail.com>
2021-11-30 03:58:46 +0000

    Adds Comment Count to values rendered on ReviewListCards

--- src/components/ReviewsList/ReviewsListCard/ReviewsListCard.tsx
+++ src/components/ReviewsList/ReviewsListCard/ReviewsListCard.tsx
@@ -12,7 +12,7 @@ interface IProps {
 }
 const ReviewsListCard: React.FC<IProps> = ({ reviewData }) => {
     const {
-        review_id, title, owner, review_img_url, category, created_at, review_votes, review_body,
+        review_id, title, owner, review_img_url, category, created_at, review_votes, review_body, comment_count
     } = reviewData;
 
     return (
@@ -21,10 +21,12 @@ const ReviewsListCard: React.FC<IProps> = ({ reviewData }) => {
             <div className="review-info1">
                 <p className="inner-review-info">Author: {owner}</p>
                 <p className="inner-review-info">Category: {formatCategoryNames(category)}</p>
+                <p className="inner-review-info">Created at: {formatCreatedAt(created_at)}</p>
             </div>
             <div className="review-info2">
-                <p className="inner-review-info">Created at: {formatCreatedAt(created_at)}</p>
+                
                 <p className="inner-review-info">Review upvotes: {review_votes}</p>
+                <p className="inner-review-info">Comment count: {comment_count}</p>
             </div>
             <div className="card-image-container">
                  <img className="card-image" src={review_img_url} alt="The game that was reviewed" />
