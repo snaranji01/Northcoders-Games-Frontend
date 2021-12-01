f81aea67fdbc20b9e84f53c9724dfd2668d5cbbd
snaranji01 <snaranji01@gmail.com>
2021-12-01 17:13:09 +0000

    disables ability to upvote comments when not signed in

--- src/components/SingleReview/Comments/CommentUpvoteButton/eventHandlers.ts
+++ src/components/SingleReview/Comments/CommentUpvoteButton/eventHandlers.ts
@@ -2,22 +2,25 @@ import { Dispatch, SetStateAction } from "react"
 import { patchCommentVotes } from "../../../../api"
 
 export const handleUpvote = (comment_id: number, setCommentUpvotes: Dispatch<SetStateAction<number>>, currentUser: any, votedUsers: string[], setVotedUsers: Dispatch<SetStateAction<string[]>>) => {
-    if (!votedUsers.includes(currentUser.username)) {
-        patchCommentVotes(1, comment_id)
-            .then(() => {
-                setCommentUpvotes(commentUpvotes => {
-                    const newCommentUpvotes = commentUpvotes + 1
-                    return newCommentUpvotes
+    if (currentUser !== null) {
+        if (!votedUsers.includes(currentUser.username)) {
+            patchCommentVotes(1, comment_id)
+                .then(() => {
+                    setCommentUpvotes(commentUpvotes => {
+                        const newCommentUpvotes = commentUpvotes + 1
+                        return newCommentUpvotes
+                    })
+                    setVotedUsers(votedUsers => {
+                        const votedUsersCopy = [...votedUsers];
+                        votedUsersCopy.push(currentUser.username)
+                        return votedUsersCopy
+                    })
                 })
-                setVotedUsers(votedUsers => {
-                    const votedUsersCopy = [...votedUsers];
-                    votedUsersCopy.push(currentUser.username)
-                    return votedUsersCopy
+                .catch(error => {
+                    console.log(error)
                 })
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        }
     }
+
 
 }
