34dcca78a70cde0acddff31f960e686779574b9e
snaranji01 <snaranji01@gmail.com>
2021-12-02 07:19:05 +0000

    import eventhandlers and adds ability to login as a listed user

--- src/components/ChooseUser/ChooseUser.tsx
+++ src/components/ChooseUser/ChooseUser.tsx
@@ -7,7 +7,7 @@ import { User } from "../../types/types";
 import { UserContext } from "../../contexts/User";
 import { AxiosError } from "axios";
 import ErrorPage from "../ErrorPage/ErrorPage";
-//import { handleChooseUserHandler } from "./eventHandlers";
+import { handleSubmitLogin, handleTextChange } from "./eventHandlers";
 
 const ChooseUser = () => {
     const [allUsers, setAllUsers] = useState<User[]>([]);
@@ -15,6 +15,8 @@ const ChooseUser = () => {
     const [error, setError] = useState<AxiosError | Error | null>(null);
 
     const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const [usernameInputText, setUsernameInputText] = useState<string>('');
 
     useEffect(() => {
         setIsLoading(true);
@@ -43,9 +45,14 @@ const ChooseUser = () => {
                         })}
                     </div>
                     <p>Please enter the name of the user you wish to login as:</p>
-                    <form>
+                    <form onSubmit={e => handleSubmitLogin(e, allUsers, setCurrentUser, usernameInputText)}>
                         <label htmlFor="type-desired-user">Username: </label>
-                        <input type="text" name="type-desired-user" id="type-desired-user" />
+                        <input
+                            type="text"
+                            name="type-desired-user"
+                            id="type-desired-user"
+                            value={usernameInputText}
+                            onChange={e => handleTextChange(e, setUsernameInputText)} />
                         <button type="submit">Sign-In</button>
                     </form>
                 </div>
